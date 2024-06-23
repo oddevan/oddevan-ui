cbe004d3db9cfeda20bdcad4f89b3fff14a40278
Evan Hildreth <me@eph.me>
2024-06-23 17:00:52 -0400

    Working on context

---
+++ src/lib/components/public/AppContext.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import type { AppTabLinkItem } from './AppTabs.svelte';
+	import type { Snippet } from 'svelte';
+	import Avatar from './Avatar.svelte';
+
+	export interface AppTabAvatarItem {
+		href: string
+		label: string
+		src: string
+	};
+
+	interface AppContextProps {
+		brand: AppTabLinkItem
+		tabs: AppTabAvatarItem[]
+		user: AppTabAvatarItem
+		children: Snippet
+	}
+
+	let { brand, tabs, user, children }: AppContextProps = $props();
+</script>
+
+<style>
+
+</style>
+
+<div>
+	<div class="appcontexttabs">
+		<a class="brand" href={brand.href}>
+			<span class="icon">
+				<svelte:component this={brand.icon} />
+			</span>
+			<span class="label">
+				{brand.label}
+			</span>
+		</a>
+		{#each tabs as {label, src, href}}
+			<a class="tab" {href}>
+				<Avatar {src} alt={label}>AB</Avatar>
+			</a>
+		{/each}
+		<a class="user" href={user.href}>
+			<Avatar src={user.src} alt={user.label}>AB</Avatar>
+		</a>
+	</div>
+	<div class="appcontextcontent">
+		{@render children()}
+	</div>
+</div>
---
+++ src/lib/components/public/Avatar.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import * as Avatar from "$lib/components/ui/avatar";
+
+	interface AvatarProps {
+		src?: string,
+		alt?: string,
+		children?: Snippet,
+	}
+
+	let {src, alt, children}: AvatarProps = $props();
+
+	let imgAtts = {};
+	if (src && !alt) {
+		imgAtts.alt = alt;
+	} else {
+		imgAtts['aria-hidden'] = 'true';
+	}
+</script>
+ 
+<Avatar.Root>
+	{#if src}
+		<Avatar.Image {src} {...imgAtts} />
+	{/if}
+	{#if children}
+		<Avatar.Fallback>
+			{@render children()}
+		</Avatar.Fallback>
+	{/if}
+</Avatar.Root>
