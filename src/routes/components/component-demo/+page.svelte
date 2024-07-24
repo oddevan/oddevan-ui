c9ecfe72fbfc737d5dbc1f3f35c54dcba4545f50
Evan Hildreth <me@eph.me>
2024-07-24 14:27:46 -0400

    Working through demo options

---
+++ src/routes/components/component-demo/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import ComponentDemo from './ComponentDemo.svelte';
+	import Button from '$lib/components/public/Button.svelte';
+
+</script>
+
+<h1>Component Demo Component</h1>
+
+<p>So now let's try a demo...</p>
+
+<ComponentDemo>
+	<Button primary action={() => console.log('Button!')}>Button</Button>
+
+<pre>
+	Code...
+</pre>
+</ComponentDemo>
---
+++ src/routes/components/component-demo/ComponentDemo.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+	import hljs from 'highlight.js/lib/core';
+	import svelte from 'highlight.js/lib/languages/svelte';
+	import 'highlight.js/styles/github.css';
+	
+	hljs.registerLanguage('svelte', svelte);
+
+	interface ComponentDemoProps {
+		children: Snippet
+	}
+
+	let { children }: ComponentDemoProps = $props();
+</script>
+
+<style>
+	.demo {
+		border: 1px solid hsl(var(--border));
+		border-radius: var(--radius);
+	}
+</style>
+
+<div class="demo">
+	{@render children()}
+</div>
